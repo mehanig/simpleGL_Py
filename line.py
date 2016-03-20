8f4be5fe01856fa74ddca7fb4724ce97a49c3d2f
mehanig <mehanig@gmail.com>
2016-03-20 16:14:37 +0300

    deltaerror version

--- line.py
+++ line.py
@@ -15,15 +15,19 @@ def line(x0, y0, x1, y1, image, color):
     if x0 > x1:
         x0, x1 = x1, x0
         y0, y1 = y1, y0
+    y = y0
+    error = 0
+    deltaerr = abs((y1 - y0) / (x1 - x0))
     for x in range(x0, x1):
-        t = (x - x0) / (x1 - x0)
-        y = int(y0 * (1. - t) + y1 * t)
-        # if transposed, de-transpose
+        # // if transposed, de-transpose
         if steep:
             image[y, x] = color
         else:
             image[x, y] = color
-
+        error += deltaerr
+        if error > 0.5:
+            y = y + 1 if y1 > y0 else y - 1
+            error -= 1.
 
 
 if __name__ == '__main__':
@@ -32,6 +36,7 @@ if __name__ == '__main__':
     # for i in range(img.size[0]):    # for every pixel:
     #     for j in range(img.size[1]):
     #         pixels[i,j] = (i, j, 100)
-    line(0, 0, 160, 160, pixels, red)
+    line(0, 0, 120, 160, pixels, red)
     line(160, 160, 120, 10, pixels, blue)
+    line(10, 230, 240, 10, pixels, green)
     ImageOps.flip(img).show()

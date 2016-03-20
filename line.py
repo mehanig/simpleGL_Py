2b004c2216de2f83240c778171f27a161d4443f4
mehanig <mehanig@gmail.com>
2016-03-20 15:51:36 +0300

    v0.1

--- line.py
+++ line.py
@@ -7,22 +7,31 @@ blue = (0, 0, 255)
 
 def line(x0, y0, x1, y1, image, color):
     steep = False
+    # if the line is steep, we transpose the image
     if abs(x0 - x1) < abs(y0 - y1):
         x0, y0 = y0, x0
         x1, y1 = y1, x1
+        steep = True
     if x0 > x1:
         x0, x1 = x1, x0
+        y0, y1 = y1, y0
     for x in range(x0, x1):
-        t = (x-x0) / (x1 - x0)
+        t = (x - x0) / (x1 - x0)
         y = int(y0 * (1. - t) + y1 * t)
-        image[x, y] = color
+        # if transposed, de-transpose
+        if steep:
+            image[y, x] = color
+        else:
+            image[x, y] = color
+
+
 
 if __name__ == '__main__':
     img = Image.new('RGB', (255, 255), "black") # create a new black image
-    # pixels = ImageOps.flip(img).load()
     pixels = img.load()
     # for i in range(img.size[0]):    # for every pixel:
     #     for j in range(img.size[1]):
     #         pixels[i,j] = (i, j, 100)
-    line(200, 200, 160, 160, pixels, red)
-    img.show()
+    line(0, 0, 160, 160, pixels, red)
+    line(160, 160, 120, 10, pixels, blue)
+    ImageOps.flip(img).show()
